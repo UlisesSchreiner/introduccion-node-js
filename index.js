6a8c7d9c6a4a3a5215d089cd25731f62a8f21512
UlisesSchreiner <schreinerulises@gmail.com>
2023-04-15 18:24:55 -0300

    modulo saludar

---
+++ index.js
@@ -0,0 +1,11 @@
+module.export = {
+  hola: function () {
+    console.log("hola mundo");
+  },
+  holaPersonalizado: function (nombre) {
+    console.log("hola", nombre);
+  },
+  holaAlCursoNPM: function () {
+    console.log("hola al curso npm");
+  },
+};
